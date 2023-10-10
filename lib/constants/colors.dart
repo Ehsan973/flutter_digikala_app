7f324facadb1aac26364e63ff382fc17760009a2
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-10-10 10:16:36 +0330

    Add BannerSlider(PageView,SmoothPageIndicator,...) and add category item(ShapeDecoration)

---
+++ lib/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class CustomColors {
+  static const Color blueIndicator = Color(0xff385EDF);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:digikala_app/constants/colors.dart';
+import 'package:digikala_app/widgets/banner_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +20,36 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text('Initial Application'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 56,
+                  width: 56,
+                  decoration: ShapeDecoration(
+                    shadows: [
+                      BoxShadow(
+                        color: Colors.amber,
+                        blurRadius: 40,
+                        spreadRadius: -6,
+                        offset: Offset(0, 15),
+                      )
+                    ],
+                    color: Colors.amber,
+                    shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.mouse,
+                    size: 32,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text('Test'),
+              ],
+            ),
           ),
         ),
       ),
